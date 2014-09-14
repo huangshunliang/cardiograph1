bd1cb68168dc33f977a8f663a7c116c42f6b6539
Noel Maersk <veox@wemakethings.net>
2014-09-14 19:00:49 +0300

    Introduce Server class, make serveraddresses list global.

--- obelisk-cardiograph.py
+++ obelisk-cardiograph.py
@@ -14,25 +14,61 @@ Author: Guillaume Aubert (gaubert) <guillaume(dot)aubert(at)gmail(dot)com>
 import zmq
 
 
+serveraddresses = ['preacher.veox.pw:9092']
+
+
+class Server:
+    """
+    """
+    def __init__(self, zmqcontext, address = 'tcp://localhost:9092'):
+        """
+        """
+        self.address = address
+        self.socket = zmqcontext.socket(zmq.SUB)
+
+    def connect(self, address = ''):
+        """
+        """
+        if address == '':
+            address = self.address
+        self.socket.connect('tcp://' + address)
+        self.socket.setsockopt(zmq.SUBSCRIBE, b'')
+
+    def disconnect(self):
+        """
+        """
+        self.socket.close()
+
+    def get_address(self):
+        """
+        """
+        return self.address
+
+    def get_heartbeat(self):
+        """
+        """
+        rawreply = self.socket.recv()
+        reply = rawreply[::-1]  # obelisk sends little-endian
+        return ':'.join(hex(x)[2:] for x in reply)
+
+
 def main():
     """ main method """
-
-    servers = ['preacher.veox.pw:9092']
-
     context = zmq.Context()
-    s = context.socket(zmq.SUB)
-    s.connect('tcp://' + servers[0])
-    s.setsockopt(zmq.SUBSCRIBE, b'')    # subscribe to everything
-
+    servers = []
+    for address in serveraddresses:
+        server = Server(context, address)
+        server.connect()
+        servers.append(server)
+        
     print("Entering main loop.")
     while True:
-        reply = s.recv()
-        reply = reply[::-1] # obelisk sent little-endian
-        data = ':'.join(hex(x)[2:] for x in reply)
-        print(servers[0], data)
+        for server in servers:
+            print(server.get_address(), server.get_heartbeat())
 
     # We never get here but clean up anyhow
-    s.close()
+    for server in servers:
+        server.disconnect()
     context.term()
 
 
